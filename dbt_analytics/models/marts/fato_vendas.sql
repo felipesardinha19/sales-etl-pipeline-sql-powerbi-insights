bcf989c84109bb1d95c78bbd3074020623669052
felipe sardinha <ofelipe461@gmail.com>
2026-05-11 16:52:40 -0300

    adicionando tabela fato e testes do dbt

---
+++ dbt_analytics/models/marts/fato_vendas.sql
@@ -0,0 +1,24 @@
+with vendas AS(
+
+	SELECT *
+	FROM {{ ref('stg_vendas') }}
+
+)
+SELECT 
+	v.id_pedido,
+	c.id_cliente,
+	p.id_produto,
+	l.id_loja,
+	t.data_venda,
+	v.qtd,
+	v.preco_unitario,
+	(v.qtd * v.preco_unitario) AS valor_total
+FROM vendas v
+LEFT JOIN {{ ref('dim_cliente') }} c
+	ON c.cliente = v.cliente
+LEFT JOIN {{ ref('dim_loja') }} l
+	ON l.loja = v.loja
+LEFT JOIN {{ ref('dim_produto') }} p
+	ON p.produto = v.produto
+LEFT JOIN {{ ref('dim_tempo') }} t
+	ON t.data_venda = v.data_venda
